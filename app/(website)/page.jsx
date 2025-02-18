dfc21cc7ca281255c13ea40eba70ba98370a64f8
harshwardhanpatil-8010 <harshwardhan.patil2005@gmail.com>
2025-02-18 22:55:37 +0530

    new

--- app/(website)/page.jsx
+++ app/(website)/page.jsx
@@ -1,14 +1,12 @@
 "use client"
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from 'next/navigation'
 export default function Home() {
   const router = useRouter()
   return (
-   <div>
-
-    <Link href="/dashboard">Dashboard</Link>
+   <div className="container p-9">
+    <Link href="/dashboard" className="border-2 border-blue-400 hover:bg-blue-900/50 transition-all duration-300 transform hover:scale-105 px-10 py-3 text-lg rounded-xl backdrop-blur-sm">Dashboard</Link>
    </div>
   );
 }
